5d1b184a2720ff6d2a54ae72b7e0f5e4dd204427
Carlos Ochoa <ochoa1604@gmail.com>
2024-09-27 15:14:12 -0600

    qr funtional

--- qr-react/src/App.jsx
+++ qr-react/src/App.jsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import QRCode from "react-qr-code";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [value, setValue] = useState(null);
+
+  const handler = (e) => {
+    e.preventDefault();
+    setValue(e.target[0].value);
+  }
 
   return (
     <>
       <div className="w-full min-h-screen grid place-content-center bg-[#181818]">
         <div className="w-[23rem] p-7 space-y-8 text-slate-300">
-          <div className="w-full h-[18rem] border border-neutral-200 rounded-3xl grid place-content-center bg-[#202020]">
+          
+          {
+            value?
+            <QRCode value={value} style={{ height: 'auto', maxWidth: '100%', width: '100%' }}/>:
+            <div className="w-full h-[18rem] border border-neutral-200 rounded-3xl grid place-content-center bg-[#202020]">
             <span>QR code</span>
           </div>
-          <form className="space-y-5">
+          }
+          <form className="space-y-5" onSubmit={handler}>
             <div>
               <input
                 className="bg-[#252525] rounded w-full p-2 focus:outline outline-neutral-300"
