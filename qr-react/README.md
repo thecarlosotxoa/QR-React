8db104254b87e3472b3f6bdbf1b987ea6b6f6b5d
Carlos Ochoa <ochoa1604@gmail.com>
2024-09-27 15:02:13 -0600

    created form and installed qr library

--- qr-react/README.md
+++ qr-react/README.md
@@ -1,8 +1,4 @@
-# React + Vite
+Simple QR code generator using the react QRCode library
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+dependencies:
+npm i react-qr-code
--- qr-react/src/App.jsx
+++ qr-react/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full min-h-screen grid place-content-center bg-[#181818]">
+        <div className="w-[23rem] p-7 space-y-8 text-slate-300">
+          <div className="w-full h-[18rem] border border-neutral-200 rounded-3xl grid place-content-center bg-[#202020]">
+            <span>QR code</span>
+          </div>
+          <form className="space-y-5">
+            <div>
+              <input
+                className="bg-[#252525] rounded w-full p-2 focus:outline outline-neutral-300"
+                placeholder="Tranforme su texto en un código QR"
+              ></input>
+            </div>
+            <button className="w-full py-3 flex justify-center bg-[#252525] hover:bg-neutral-600 duration-200 rounded">
+              Crear
+            </button>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
